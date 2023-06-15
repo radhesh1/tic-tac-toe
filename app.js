9ebdffb41a0f64f6927538af18c39cc7cca0a6e8
radhesh1 <70819367+radhesh1@users.noreply.github.com>
2023-06-16 02:17:52 +0530

    functionality

--- app.js
+++ app.js
@@ -37,4 +37,21 @@ function checkScore(){
         [0,3,6],[1,4,7],[2,5,8],
         [0,4,8],[2,4,6]
     ]
+    winningCombos.forEach(array =>{
+        const circleWins = array.every(cell => allSquares[cell].firstChild?.classList.contains('circle'))
+        if(circleWins){
+            infoDisplay.textContent = "Circle Wins!!"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+    })
+
+    winningCombos.forEach(array =>{
+        const crossWins = array.every(cell => allSquares[cell].firstChild?.classList.contains('cross'))
+        if(crossWins){
+            infoDisplay.textContent = "Cross Wins!!"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+    })
 }
