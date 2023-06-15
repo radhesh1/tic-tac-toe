b7ee2cce4df2f92071140bb98f639f6387b3187a
radhesh1 <70819367+radhesh1@users.noreply.github.com>
2023-06-16 02:10:05 +0530

    add functionality

--- app.js
+++ app.js
@@ -26,4 +26,15 @@ function addGo(e){
     go = go === "circle" ? "cross" : "circle"
     infoDisplay.textContent = "it is now" + go +"'s go."
     e.target.removeEventListener('click',addGo)
+    checkScore()
+}
+
+function checkScore(){
+    const allSquares = document.querySelectorAll('.square')
+    console.log(allSquares)
+    const winningCombos = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
 }
