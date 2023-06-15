167cbd404f34499f7363432cc288d497bbab176b
radhesh1 <70819367+radhesh1@users.noreply.github.com>
2023-06-16 01:27:45 +0530

    addition of js and css data

---
+++ app.js
@@ -0,0 +1,13 @@
+const gameBoard = document.querySelector('#gameboard')
+const infoDisplay =document.querySelector('#info')
+const startCells = [
+    "", "", "", "", "", "", "", "", ""
+]
+
+function createBoard(){
+    startCells.forEach((cell, index) =>{
+       const cellElement = document.createElement('div')
+       cellElement.classList.add('square')
+       gameBoard.append(cellElement)
+    })
+}
